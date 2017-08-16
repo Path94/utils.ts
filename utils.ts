fc48fdf75580edf8b28d8ff21cb35220256c6635
itsMontoya <josh@montoya.io>
2017-08-16 11:02:05 -0700

    Add IsContainedWithin

--- utils.ts
+++ utils.ts
@@ -1,5 +1,16 @@
 import * as errors from "node_modules/errors.js/errors"
 
+// Response is a common response struct-type
+export class Response<T> {
+    Value: T;
+    Error: errors.Error | null;
+
+    constructor(value: T, err: errors.Error | null) {
+        this.Value = value;
+        this.Error = err;
+    }
+}
+
 // RemoveElement will safely remove an element from the DOM
 export function RemoveElement(e: HTMLElement) {
 	const p = e.parentNode;
@@ -10,12 +21,16 @@ export function RemoveElement(e: HTMLElement) {
 	p.removeChild(e);
 }
 
-export class Response<T> {
-    Value: T;
-    Error: errors.Error | null;
+// IsContainedWithin will detect if an element is contained within a parent
+export function IsContainedWithin(parent: HTMLElement, child: HTMLElement): boolean {
+	while (child.parentElement !== null) {
+		if (parent === child.parentElement) {
+			return true;
+		}
 
-    constructor(value: T, err: errors.Error | null) {
-        this.Value = value;
-        this.Error = err;
-    }
+		child = child.parentElement;
+	}
+
+	return false;
 }
+
