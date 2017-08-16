f85890a5d219c2f1367f6f5a4d99b841dca2864a
itsMontoya <josh@montoya.io>
2017-08-16 09:43:26 -0700

    Move response to utils.ts, add removeElement func

--- utils.ts
+++ utils.ts
@@ -1,5 +1,15 @@
 import * as errors from "node_modules/errors.js/errors"
 
+// RemoveElement will safely remove an element from the DOM
+export function RemoveElement(e: HTMLElement) {
+	const p = e.parentNode;
+	if (p === null) {
+		return;
+	}
+
+	p.removeChild(e);
+}
+
 export class Response<T> {
     Value: T;
     Error: errors.Error | null;
